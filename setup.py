ff7adf72dc2b5bf0634406d10019aa6d65a59ba5
daniiarkg <daniiar.commerce@gmail.com>
2021-03-20 19:24:19 +0600

    upd0.5

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ilim',         # How you named your package folder (MyLib)
   packages = ['ilim'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license = 'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Grammar formater for kyrgyz language',   # Give a short description about your library
   author = 'Daniyar Biybosunov',                   # Type in your name
   author_email = 'daniiar.commerce@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/daniiarkg/ilim',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/daniiarkg/ilim/archive/refs/tags/v_0.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/daniiarkg/ilim/archive/refs/tags/v_0.5.tar.gz',    # I explain this later on
   keywords = ['Grammar', 'Formatter', 'Grammar formatter', 'Library'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
