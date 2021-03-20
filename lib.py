ade5e0714ef01381e26418642e92b1defbc16830
daniiarkg <daniiar.commerce@gmail.com>
2021-03-20 17:35:41 +0600

    alpha rel.

---
+++ lib.py
@@ -0,0 +1,81 @@
+import logging
+from storage import *
+
+
+
+def check_number(request):
+    if False in list(map(lambda x: x in nums, request[0])): 
+        logging.critical('Something went wrong! Probably non-number character found in your request string!')
+
+
+
+def check_kirill(request):
+    error_list = cons + vowl
+    if request[-1] not in error_list and request[-2] not in error_list:
+        logging.critical('Something went wrong! Probably nsupported character found in your request string!')
+
+
+
+def make_numb(request):
+    check_number(request)
+    str_req = request[0]
+    stack_num = []
+    for i,j in zip( str_req[::-1], list(range(0,len(str_req))) ):
+        if j == 0:
+            stack_num.append('нөл' if len(str_req) < 2 and int(i) == 0 else numb_dict_list[0][int(i)])
+        else:
+            stack_num.append(numb_dict_list[j][int(i)])
+    stack_num = list(filter(lambda a: a, stack_num[::-1]))
+    str_req = ' '.join(stack_num)
+    if 'миллиард' in str_req or 'миллион' in str_req and int(request[0][::-1][3:6]) == 0:
+        str_req = str_req.replace(' миң','')
+    if request[1][-1] == 's':
+        pass
+    return str_req
+    
+    
+
+def make_plural(request):
+    check_kirill(request[0])
+    plur_str = ''
+    for i in plur_dict_list[0]:
+        if request[0][-1] in i:
+            plur_str += plur_dict_list[0][i]
+
+    for i in plur_dict_list[1]:
+        if request[0][-2 if request[0][-1] not in vowl else -1] in i:
+            plur_str += plur_dict_list[1][i]
+            
+    return request[0] + plur_str
+
+def make_case(request):
+    check_kirill(request[0])
+    try:
+        num_case = int(request[1][3])
+        r_1_2 = request[0][-1] in vowl
+        if request[0][-1] in 'гГ' and num_case == 2:
+            request[0] = request[0][:-1] + 'к'
+        need_sog = 'н' if r_1_2 and num_case == 1 else 'г' if request[0][-1] in g_cons + vowl and num_case == 2 else 'к' if request[0][-1] in g_cons and num_case == 2 else 'д' if request[0][-1] in z_cons + vowl else 'т' 
+        check_let = request[0][-1] if r_1_2 else request[0][-2]
+        for i in case_dict_list[num_case - 1]:
+            if check_let in i:
+                return request[0] + need_sog + case_dict_list[num_case - 1][i]
+    except:
+        logging.critical('Something went wrong! Check your request string!')
+
+
+
+router = {
+    'm-c' : make_case,
+    'm-p' : make_plural,
+    'm-n' : make_numb
+}
+
+
+
+def make_format(got_request, route):
+    request = [got_request, route]
+    try:
+        return router[request[1][:3]](request)
+    except:
+        logging.critical('Something went wrong! Check your route or func call!')
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'ilim',         # How you named your package folder (MyLib)
+  packages = ['ilim'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license = 'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Grammar formater for kyrgyz language',   # Give a short description about your library
+  author = 'Daniyar Biybosunov',                   # Type in your name
+  author_email = 'daniiar.commerce@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/daniiarkg/ilim',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/daniiarkg/ilim/archive/refs/tags/v_0.1.tar.gz',    # I explain this later on
+  keywords = ['Grammar', 'Formatter', 'Grammar formatter', 'Library'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
