f3e4a1f374feedea9068008e0e9dec08c536f381
daniiarkg <daniiar.commerce@gmail.com>
2021-04-01 00:09:48 +0600

    0.6alpha

--- ilim/formater.py
+++ ilim/formater.py
@@ -1,198 +1,17 @@
 import logging
+from storage import *
 
-numb_s_case_dict_list = {
-    1 : '',
-    2 : '',
-    3 : '',
-    4 : '',
-    5 : '',
-    6 : '',
-    7 : '',
-    8 : '',
-    9 : '',
-    0 : '', 
-}
+class FuncCallException(Exception):
+    pass
 
-numb_dict_list = [
-        
-    {
-        1 : 'бир',
-        2 : 'эки',
-        3 : 'үч',
-        4 : 'төрт',
-        5 : 'беш',
-        6 : 'алты',
-        7 : 'жети',
-        8 : 'сегиз',
-        9 : 'тогуз',
-        0 : '',
-    },
+call_stack = 0
 
-    {
-        1 : 'он',
-        2 : 'жыйырма',
-        3 : 'отуз',
-        4 : 'кырк',
-        5 : 'элүү',
-        6 : 'алтымыш',
-        7 : 'жетимиш',
-        8 : 'сексен',
-        9 : 'токсон',
-        0 : ''
-    },
-
-    {
-        1 : 'жүз',
-        2 : 'эки жүз',
-        3 : 'үч жүз',
-        4 : 'төрт жүз',
-        5 : 'беш жүз',
-        6 : 'алты жүз',
-        7 : 'жети жүз',
-        8 : 'сегиз жүз',
-        9 : 'тогуз жүз',
-        0 : ''
-    },
-
-    {
-        1 : 'бир миң',
-        2 : 'эки миң',
-        3 : 'үч миң',
-        4 : 'төрт миң',
-        5 : 'беш миң',
-        6 : 'алты миң',
-        7 : 'жети миң',
-        8 : 'сегиз миң',
-        9 : 'тогуз миң',
-        0 : 'миң'
-    },
-
-        {
-        1 : 'он',
-        2 : 'жыйырма',
-        3 : 'отуз',
-        4 : 'кырк',
-        5 : 'элүү',
-        6 : 'алтымыш',
-        7 : 'жетимиш',
-        8 : 'сексен',
-        9 : 'токсон',
-        0 : ''
-    },
-
-    {
-        1 : 'жүз',
-        2 : 'эки жүз',
-        3 : 'үч жүз',
-        4 : 'төрт жүз',
-        5 : 'беш жүз',
-        6 : 'алты жүз',
-        7 : 'жети жүз',
-        8 : 'сегиз жүз',
-        9 : 'тогуз жүз',
-        0 : ''
-    },
-
-    {
-        1 : 'бир миллион',
-        2 : 'эки миллион',
-        3 : 'үч миллион',
-        4 : 'төрт миллион',
-        5 : 'беш миллион',
-        6 : 'алты миллион',
-        7 : 'жети миллион',
-        8 : 'сегиз миллион',
-        9 : 'тогуз миллион',
-        0 : ''
-    },
-
-    {
-        1 : 'бир миллиард',
-        2 : 'эки миллиард',
-        3 : 'үч миллиард',
-        4 : 'төрт миллиард',
-        5 : 'беш миллиард',
-        6 : 'алты миллиард',
-        7 : 'жети миллиард',
-        8 : 'сегиз миллиард',
-        9 : 'тогуз миллиард',
-        0 : ''
-    },
-
-]
-
-plur_dict_list = [
-
-    {
-        'аеёиоуыэюяөүАЕЁИОУЫЭЮЯӨҮ' : 'л',
-        'шШщЩхХфФсСцЦчЧтТпПкК' : 'т',
-        'гГңҢдДбБвВжЖзЗйЙлЛрРмМнН' : 'д',
-    },
-
-    {
-        
-        'ыЫаАяЯ' : 'ар',
-        'иИеЕэЭ' : 'ер',
-        'юЮёЁоОуУ' : 'ор',
-        'өӨүҮ' : 'өр',
-
-    },
-
-]
-
-case_dict_list = [
-    
-    {
-        'ыЫаАяЯ' : 'ын',
-        'иИеЕэЭ' : 'ин',
-        'юЮёЁоОуУ' : 'ун',
-        'өӨүҮ' : 'үн',
-
-    },
-
-    {
-        
-        'ыЫаАяЯ' : 'а',
-        'иИеЕэЭ' : 'е',
-        'юЮёЁоОуУ' : 'о',
-        'өӨүҮ' : 'ө',
-
-    },
-
-    {
-        
-        'ыЫаАяЯ' : 'ы',
-        'иИеЕэЭ' : 'и',
-        'юЮёЁоОуУ' : 'у',
-        'өӨүҮ' : 'ү',
-
-    },
-
-    {
-        
-        'ыЫаАяЯ' : 'ан',
-        'иИеЕэЭ' : 'ен',
-        'юЮёЁоОуУ' : 'он',
-        'өӨүҮ' : 'өн',
-
-    },
-
-    {
-        
-        'ыЫаАяЯ' : 'а',
-        'иИеЕэЭ' : 'е',
-        'юЮёЁоОуУ' : 'о',
-        'өӨүҮ' : 'ө',
-
-    },
-
-]
-
-vowl = 'аеёиоуыэюяөүАЕЁИОУЫЭЮЯӨҮ'
-cons = 'шШщЩхХфФсСцЦчЧтТпПкКңҢдДбБвВжЖзЗйЙлЛрРмМнН'
-z_cons = 'ңҢдДбБвВжЖзЗйЙлЛрРмМнН'
-g_cons = 'шШщЩхХфФсСцЦчЧтТпПкК'
-nums = '0123456789'
+def check_call_stack():
+    global call_stack
+    if call_stack < 1:
+        logging.critical('Invalid function call!')
+        raise FuncCallException
+    call_stack -= 1
 
 def check_number(request):
     if False in list(map(lambda x: x in nums, request[0])): 
@@ -208,6 +27,9 @@ def check_kirill(request):
 
 
 def make_numb(request):
+    check_call_stack()
+    global call_stack
+    call_stack += 1
     check_number(request)
     str_req = request[0]
     stack_num = []
@@ -227,6 +49,9 @@ def make_numb(request):
     
 
 def make_plural(request):
+    check_call_stack()
+    global call_stack
+    call_stack += 1
     check_kirill(request[0])
     plur_str = ''
     for i in plur_dict_list[0]:
@@ -240,6 +65,9 @@ def make_plural(request):
     return request[0] + plur_str
 
 def make_case(request):
+    check_call_stack()
+    global call_stack
+    call_stack += 1
     check_kirill(request[0])
     try:
         num_case = int(request[1][3])
@@ -265,8 +93,17 @@ router = {
 
 
 def make_format(got_request, route):
+    global call_stack
+    call_stack += 1
     request = [got_request, route]
     try:
         return router[request[1][:3]](request)
     except:
         logging.critical('Something went wrong! Check your route or func call!')
+
+
+if __name__ == "__main__":
+    import sys
+    requestConsole = sys.argv[1:]
+    if len(requestConsole) != 2 : logging.critical('Something went wrong! Check your route or func call!')
+    print(make_format(requestConsole[0], requestConsole[1]))
